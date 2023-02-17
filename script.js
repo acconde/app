c19643e23d23cdb5a9d8758aca8639151f80e0f3
acconde <114787853+acconde@users.noreply.github.com>
2023-02-17 21:49:17 +0800

    Final Crypto App list

--- script.js
+++ script.js
@@ -1,87 +1,135 @@
-const searchInput = document.getElementById('search');
-const row1 = document.getElementById('row1');
-const row2 = document.getElementById('row2');
-const row3 = document.getElementById('row3');
-const icons = document.getElementById('icons');
-const notification = document.getElementById('notification');
+const apiUrl = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd';
 
 let page = 1;
+let loading = false;
+let end = false;
+let searchQuery = '';
 
-async function getRates() {
-  try {
-    const response = await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Ccardano%2Cbinancecoin%2Cripple%2Csolana%2Cdogecoin%2Cpolkadot%2Cshiba-inu%2Cavalanche&vs_currencies=usd&include_24hr_change=true`);
+const cardContainer = document.getElementById('card-container');
+const loadingIndicator = document.getElementById('loading');
+const endIndicator = document.getElementById('end');
+const errorIndicator = document.getElementById('error');
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
+const image = document.createElement('img');
+
+
+
+async function loadMore() {
+  if (loading || end) {
+    return;
+  }
+
+  loading = true;
+  loadingIndicator.style.display = 'block';
+
+  let url = `${apiUrl}&page=${page}`;
+
+  if (searchQuery) {
+    const searchUrl = `https://api.coingecko.com/api/v3/coins/list?include_platform=false`;
+    const response = await fetch(searchUrl);
     const data = await response.json();
-    displayRates(data);
-  } catch (error) {
-    console.error(error);
+
+    const filteredData = data.filter(coin => coin.name.toLowerCase().includes(searchQuery.toLowerCase()));
+
+    if (filteredData.length === 0) {
+      loadingIndicator.style.display = 'none';
+      errorIndicator.style.display = 'block';
+      return;
+    }
+
+    url += `&ids=${filteredData.map(coin => coin.id).join(',')}`;
   }
-}
 
-function displayRates(rates) {
-  if (Object.keys(rates).length === 0) {
-    notification.textContent = 'No more records to show';
-  } else {
-    for (let i = 0; i < Object.keys(rates).length; i++) {
-      const card = document.createElement('div');
-      card.classList.add('card');
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      loading = false;
+      loadingIndicator.style.display = 'none';
 
-      const icon = document.createElement('img');
-      icon.src = `https://cryptoicons.org/api/color/${Object.keys(rates)[i]}/200`;
-
-      const name = document.createElement('h2');
-      name.textContent = Object.keys(rates)[i];
-
-      const price = document.createElement('p');
-      price.textContent = `Price: ${rates[Object.keys(rates)[i]].usd} USD`;
-
-      const change = document.createElement('p');
-      change.textContent = `24h Change: ${rates[Object.keys(rates)[i]].usd_24h_change.toFixed(2)}%`;
-
-      card.appendChild(icon);
-      card.appendChild(name);
-      card.appendChild(price);
-      card.appendChild(change);
-
-      if (i < 3) {
-        icons.appendChild(icon);
+      if (data.length === 0) {
+        if (searchQuery) {
+          errorIndicator.style.display = 'block';
+        } else {
+          end = true;
+          endIndicator.style.display = 'block';
+        }
+        return;
       }
 
-      if (i >= 3 && i < 6) {
-        row1.appendChild(card);
-      } else if (i >= 6 && i < 9) {
-        row2.appendChild(card);
-      } else {
-        row3.appendChild(card);
+      for (const item of data) {
+        const card = document.createElement('div');
+        card.classList.add('card', 'mb-3');
+        card.style.maxWidth = '540px';
+        const row = document.createElement('div');
+        row.classList.add('row', 'g-0');
+        const imageWrapper = document.createElement('div');
+        imageWrapper.classList.add('col-md-4');
+        const image = document.createElement('img');
+        image.src = item.image;
+        image.classList.add('img-fluid', 'rounded-start', 'card-image');
+        image.alt = `${item.name} logo`;
+        const contentWrapper = document.createElement('div');
+        contentWrapper.classList.add('col-md-8');
+        const content = document.createElement('div');
+        content.classList.add('card-body');
+        const title = document.createElement('h5');
+        title.classList.add('card-title');
+        title.textContent = `Crypto Title: ${item.name}`;
+        const rate = document.createElement('div');
+        rate.classList.add('card-rate-label');
+        rate.textContent = `Rate: ${item.current_price} USD`;
+        const unit = document.createElement('div');
+        unit.classList.add('card-unit');
+        unit.textContent = `Crypto Unit: ${item.symbol.toUpperCase()}`;
+
+        imageWrapper.appendChild(image);
+        content.appendChild(rate);
+        content.appendChild(title);
+        content.appendChild(unit);
+        contentWrapper.appendChild(content);
+        row.appendChild(imageWrapper);
+        row.appendChild(contentWrapper);
+        card.appendChild(row);
+        cardContainer.appendChild(card);
       }
-    }
-  }
+
+      page++;
+    })
+    .catch(() => {
+      loading = false;
+      loadingIndicator.style.display = 'none';
+      errorIndicator.style.display = 'block';
+    });
 }
 
-searchInput.addEventListener('input', () => {
-  const query = searchInput.value.toLowerCase();
-  const cards = document.querySelectorAll('.card');
 
-  cards.forEach(card => {
-    const name = card.querySelector('h2').textContent.toLowerCase();
-    if (name.includes(query)) {
-      card.style.display = 'block';
-    } else {
-      card.style.display = 'none';
-    }
-  });
+function search() {
+  page = 1;
+  end = false;
+  errorIndicator.style.display = 'none';
+  cardContainer.innerHTML = '';
+  searchQuery = searchInput.value.trim();
+  loadMore();
+}
+
+
+searchInput.addEventListener('keyup', (event) => {
+  if (event.key === 'Enter') {
+    search();
+  }
 });
 
-window.addEventListener('load', () => {
-  notification.textContent = 'Loading data...';
-  getRates();
-});
+searchButton.addEventListener('click', search);
 
 window.addEventListener('scroll', () => {
   const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
 
   if (scrollTop + clientHeight >= scrollHeight - 5) {
-    page++;
-    notification.textContent = 'Loading more data...';
-    getRates();
+    loadMore();
   }
 });
+
+loadMore();
+
+
